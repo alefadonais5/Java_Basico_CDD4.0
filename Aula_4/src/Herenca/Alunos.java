f23f8f998c6e988edf65270a38ddccadc669bdde
Alef Adonais <alefadonais5@gmail.com>
2023-07-13 11:16:38 -0300

    POO
    POO

---
+++ Aula_4/src/Herenca/Alunos.java
@@ -0,0 +1,14 @@
+package Herenca;
+
+import java.util.Date;
+
+public class Alunos extends Pessoa
+{
+	public Alunos(String _nome, String _cpf, Date _data) 
+	{
+		super(_nome, _cpf, _data);
+	}
+	public String matrícula;
+}
+
+
---
+++ Aula_4/src/Metodos/Aplicacao_Honda.java
@@ -0,0 +1,11 @@
+package Metodos;
+
+public class Aplicacao_Honda {
+
+	public static void main(String[] args) {
+		Honda hondaFitPreto = new Honda("2.0 Flex", "Honda Accord", 60000);
+		
+
+	}
+
+}
